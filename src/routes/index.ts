f302be004f30b790d6ed7fdf93443d3946cedfb7
HakimMohamed <kemstroarab@gmail.com>
2024-12-02 09:08:29 +0200

    fix missing address route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import authRoutes from './auth';
 import itemsRoutes from './item';
 import cartRoutes from './cart';
+import addressRoutes from './address';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/items', itemsRoutes);
 router.use('/cart', cartRoutes);
+router.use('/address', addressRoutes);
 
 export default router;
